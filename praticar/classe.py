6b5b400348f124c2991b90590fab3f3aead8cf8d
ISAAC KRUTZSCH ZOCCATELLI <isaac_zoccatelli@estudante.sesisenai.org.br>
2026-02-11 11:16:46 -0300

    Pequena prática de POO

---
+++ praticar/classe.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self, porte, raca, cor):
+        self.porte = porte
+        self.raca = raca
+        self.cor = cor
+
+def mostrar_info(self):
+    print("Informações dos animais:")
+    print(f"Porte: {self.porte}")
+    print(f"Raça: {self.raca}")
+    print(f"Cor: {self.cor}")
+
+# criando objeto da classe
+katy = Animal(porte = "Pequeno", raca = "Vira lata", cor = "Preto")
+jorge = Animal(porte = "Grande", raca = "Golden Retriever", cor = "Dourado esbranquiçado")
+
+mostrar_info(jorge)
+katy.mostrar_info()
