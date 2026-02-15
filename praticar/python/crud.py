6ad908c49040ad7a7a13b0d356d9e632fc0838b8
Isaac <isaackz2008@gmail.com>
2026-02-15 00:19:50 -0300

    CRUD de gerenciamento de turma FINALIZADO!

--- praticar/python/crud.py
+++ praticar/python/crud.py
@@ -3,22 +3,55 @@ arquivo python pra eu tentar fazer crud sozinho (sem inteligencia artificial)
 tentarei fazer um gerenciamento de notas de um aluno, depois avançarei para uma turma.
 """
 
-aluno_notas = []
 turma = []
-
-def add_aluno():
-    aluno = {}
-    aluno["notas"] = []
-    aluno["nome"] = input("Digite o nome do novo aluno: ")
-    turma.append(aluno)
 
 def ver_turma(): # mostra as notas
     if len(turma) > 0:
         print('\n-- NOTAS --')
         for i in range(len(turma)):
-            print(f'{i+1}° - {turma[i]}')
+            nome = turma[i]['nome']
+            notas = turma[i]['notas']
+            print(f'{i+1}° - {nome} | Notas: {notas}')
     else:
         print("\nNenhuma nota existente!")
+
+def add_aluno():
+    aluno = {}
+    aluno["nome"] = input("Digite o nome do novo aluno: ")
+    aluno["notas"] = []
+    turma.append(aluno)
+
+def remover_aluno():
+    try:
+        if len(turma) >= 1:
+            ver_turma()
+            escolha = int(input("Escolha qual aluno deseja remover?"))
+            confirmacao = input("Tem certeza que deseja remover esse aluno? S/N").upper()
+            if confirmacao == "S":
+                if escolha >= 0:
+                    turma.pop(escolha - 1)
+                    print("Aluno excluido com sucesso!")
+                    return
+            elif confirmacao == "N":
+                return
+            else:
+                print("Resposta inválida!")
+    except ValueError:
+        print("Valor inválido")
+    except IndexError:
+        print("Aluno inexistente!")
+
+def atualizar_aluno():
+    try:
+        if len(turma) >= 1:
+            ver_turma()
+            escolha = int(input("Qual aluno deseja atualizar o nome? (número)"))
+            nome_novo = input("Digite o novo nome do aluno!")
+            turma[escolha - 1]["nome"] = nome_novo
+    except ValueError:
+        print("Valor inválido!")
+    except IndexError:
+        print("Aluno inexistente!")
 
 def adicionar_nota():
     try:
@@ -30,41 +63,47 @@ def adicionar_nota():
             aluno_escolhido = int(input("Escolha o aluno que quer adicionar uma nota: "))
             nota = float(input("Adicione uma nova nota: "))
             if 0 <= nota <= 10: # se a nota for válida prossegue
-                notas = turma[aluno_escolhido]["notas"]
+                notas = turma[aluno_escolhido - 1]["notas"]
                 notas.append(nota) # add nota na lista
                 print("Nota adicionada com sucesso!")
             else:
                 print('nota inválida! tente novamente')
     except ValueError:
         print("Valor inválido!")
+    except IndexError:
+        print("Nota inexistente!") 
 
 def atualizar_nota():
     ver_turma()
     try:
         if len(turma) >= 1:
-            ver_turma()
             aluno_escolhido = int(input("Escolha o aluno que quer atualizar uma nota: "))
+            print(turma[aluno_escolhido - 1]["notas"])
             escolha = int(input(f"Escolha uma nota para atualizar: \n"))
             nova_nota = float(input("Digite a nova nota: "))
             if nova_nota >= 0 and nova_nota <= 10:
-                nota = turma[aluno_escolhido]["notas"]
+                nota = turma[aluno_escolhido - 1]["notas"]
                 nota[escolha - 1] = nova_nota
-            print("Nota atualizada! \n") 
+                print("Nota atualizada! \n") 
     except ValueError:
         print("Valor inválido!")
     except IndexError:
         print("Nota inexistente!")
 
 def excluir_nota():
-    ver_turma()
     try:
-        aluno_escolhido = int(input("Escolha o aluno que quer atualizar uma nota: "))
-        print("Notas do aluno " , turma[aluno_escolhido])
-        print(turma[aluno_escolhido]["notas"])
-        escolha = int(input(f"Escolha uma nota para excluir: \n"))
-        notas = turma[aluno_escolhido]["notas"]
-        notas.pop(escolha - 1) # -1 pois index de python começa em 0
-        print(f"Nota excluída! \n")
+        if len(turma) >= 1:
+            ver_turma()
+            aluno_escolhido = int(input("Escolha o aluno que quer atualizar uma nota: "))
+            print("Notas do aluno " , turma[aluno_escolhido - 1])
+            print(turma[aluno_escolhido - 1]["notas"])
+            escolha = int(input(f"Escolha uma nota para excluir: \n"))
+            notas = turma[aluno_escolhido - 1]["notas"]
+            notas.pop(escolha - 1) # -1 pois index de python começa em 0
+            print(f"Nota excluída! \n")
+        else:
+            print("Nenhuma nota existente!")
+            return
     except ValueError:
         print("Valor inválido!")
     except IndexError:
@@ -74,28 +113,32 @@ def menu():
     while True:
         print('\n--- MENU ---')
         try:
-            print('1 - Ver notas')
-            print('2 - Adicionar nota')
-            print('3 - Atualizar nota')
-            print('4 - Excluir nota')
-            print('5 - Encerrar')
+            print('1 - Ver nomes e notas')
+            print('2 - Adicionar aluno')
+            print('3 - Remover aluno')
+            print('4 - Atualizar Aluno')
+            print('5 - Adicionar nota')
+            print('6 - Atualizar nota')
+            print('7 - Excluir nota')
+            print('8 - Encerrar programa')
             escolha = int(input('Escolha uma opção (número): '))
 
             if escolha == 1:
                 ver_turma()
-
             elif escolha == 2:
-                adicionar_nota()
-
+                add_aluno()
             elif escolha == 3:
-                atualizar_nota()
-
+                remover_aluno()
             elif escolha == 4:
-                excluir_nota()
-
+                atualizar_aluno()
             elif escolha == 5:
+                adicionar_nota()
+            elif escolha == 6:
+                atualizar_nota()
+            elif escolha == 7:
+                excluir_nota()
+            elif escolha == 8:
                 break
-
             else:
                 print("Opção inválida!")
     
