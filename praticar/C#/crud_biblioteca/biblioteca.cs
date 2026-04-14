cb60c458d69cf6879807f9b62cbc5e8115455ab3
Isaac <isaackz2008@gmail.com>
2026-04-14 15:34:34 -0300

    tendo que dar commit em UM COMENTARIO por causa de conflict.

--- praticar/C#/crud_biblioteca/biblioteca.cs
+++ praticar/C#/crud_biblioteca/biblioteca.cs
@@ -2,8 +2,8 @@
 
 class Livro {
     public int id;
-    public string? titulo;
-    public string? autor;
+    public string? titulo; // o "?" é para assumir o risco do valor ser nulo. 
+    public string? autor; // porque caso não haja o "?", o compilador irá ficar alertando o possível erro (valor ser nulo)
     public string? genero;
     public bool disponivel;
 }
