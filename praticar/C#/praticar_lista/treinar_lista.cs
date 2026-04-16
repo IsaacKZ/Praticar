c3c8393aee430c7718723e58b81f236dc49dd708
ISAAC KRUTZSCH ZOCCATELLI <isaac_zoccatelli@estudante.sesisenai.org.br>
2026-04-16 11:03:58 -0300

    CRUD para treinar Lista em C#

--- praticar/C#/praticar_lista/treinar_lista.cs
+++ praticar/C#/praticar_lista/treinar_lista.cs
@@ -1,14 +1,113 @@
 // arquivo pra eu treinar lista em C#
 
-// adicionando nomes na lista
-List<string> pessoas = new List<string>();
+List<string> pessoas = new List<string>(); // declarando variavel lista
+bool rodando = true;
 
-for (int i=0; i < 5; i++) {
-    Console.WriteLine("Digite o nome da proxima pessoa a ser adicionada:");
-    string nova_pessoa = Console.ReadLine()!;
-    pessoas.Add(nova_pessoa);
+void listar_pessoas() // Mostrar todas as pessoas na lista
+{
+    for (int i = 0; i < pessoas.Count; i++)
+    {
+        Console.WriteLine($"{i + 1} - {pessoas[i]}");
+    }
 }
 
-for (int i=0; i < pessoas.Count; i++) {
-    Console.WriteLine($"{i + 1} - {pessoas[i]}");
+int selecionar_pessoa()
+{
+    listar_pessoas();
+    int pos = int.Parse(Console.ReadLine());
+    return pos;
+}
+
+void adicionar_pessoas()  // adicionar pessoas na lista 
+{
+    Console.WriteLine("Digite o nome da proxima pessoa a ser adicionada:"); // adicionando nomes na lista
+    string nova_pessoa = Console.ReadLine()!;
+    pessoas.Add(nova_pessoa);
+    Console.WriteLine("Pessoa adicionada com sucesso! Lista atualizada:");
+    listar_pessoas();
+}
+
+void remover_pessoas() // Remover pessoas da lista
+{
+    Console.WriteLine("Digite o número da pessoa que deseja remover: ");
+    int pos = selecionar_pessoa();
+
+    if (pos > pessoas.Count || pos < 1)
+    {
+        Console.WriteLine("Erro! Retornando ao menu...");
+        return;
+    }
+    else
+    {
+        pessoas.RemoveAt(pos - 1);
+        Console.WriteLine("Pessoa removida com sucesso! Lista Atualizada: ");
+        listar_pessoas();
+    }
+}
+
+void modificar_pessoa() // Modificar uma pessoa existente na lista
+{
+    int pos = selecionar_pessoa();
+    Console.WriteLine("Escolha o número da pessoa que deseja alterar");
+    if (pos > pessoas.Count || pos < 1)
+    {
+        Console.WriteLine("Erro! Retornando ao menu...");
+        return;
+    }
+
+    else
+    {
+        Console.WriteLine("Digite o novo nome da pessoa: (Digite 0 para cancelar) ");
+        string novo_nome = Console.ReadLine();
+
+        if (novo_nome != "0")
+        {
+            pessoas[pos - 1] = novo_nome;
+            Console.WriteLine("Pessoa modificada com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Retornando ao menu...");
+            return;
+        }
+    }
+}
+
+void menu() // Menu para escolher o que fazer
+{
+    Console.WriteLine("--- MENU ---");
+    Console.WriteLine("1 - Adicionar pessoas");
+    Console.WriteLine("2 - Remover pessoas");
+    Console.WriteLine("3 - Modificar pessoas");
+    Console.WriteLine("4 - Listar pessoas");
+    Console.WriteLine("5 - Sair do programa");
+    int escolha = int.Parse(Console.ReadLine());
+
+    switch (escolha)
+    {
+        case 1:
+            adicionar_pessoas();
+            break;
+
+        case 2:
+            remover_pessoas();
+            break;
+
+        case 3:
+            modificar_pessoa();
+            break;
+
+        case 4:
+            listar_pessoas();
+            break;
+
+        case 5:
+            rodando = false;
+            break;
+    }
+}
+
+while (rodando == true)
+{
+    menu();
 }
